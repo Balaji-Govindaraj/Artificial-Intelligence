b1a2dadf0384e8bf3c511fd0ebaa2fed94a17b9f
Balaji-Govindaraj <baluthegreat2@gmail.com>
2018-04-22 22:16:44 +0530

    Update reinforcement_learning_cartpole.py
    Reinforcement learning on cartpole game:
        (i) Dynamic policies built using Neural Network Polices
        (ii) Actions using Credit Assignment Problem
        (iii) Policy Gradient along with gradient descent

--- reinforcement_learning_cartpole.py
+++ reinforcement_learning_cartpole.py
@@ -8,7 +8,7 @@ import numpy as np
 def discount_rewards(rewards,discount_rate):
 	discounted_rewards=np.empty(len(rewards))
 	cumulative_rewards=0
-	for	step in	reversed(range(len(rewards))):
+	for step in reversed(range(len(rewards))):
 		cumulative_rewards=rewards[step]+cumulative_rewards*discount_rate
 		discounted_rewards[step]=cumulative_rewards
 	return	discounted_rewards
@@ -32,20 +32,20 @@ outputs=tf.nn.sigmoid(logits)
 p_left_and_right=tf.concat(axis=1,values=[outputs,1-outputs])
 action=tf.multinomial(tf.log(p_left_and_right),	num_samples=1)
 y=1.-tf.to_float(action)
-cross_entropy=tf.nn.sigmoid_cross_entropy_with_logits(labels=y,	logits=logits)
+cross_entropy=tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=logits)
 optimizer=tf.train.AdamOptimizer(learning_rate)
 grads_and_vars=optimizer.compute_gradients(cross_entropy)
-gradients=[grad	for	grad,	variable	in	grads_and_vars]
+gradients=[grad	for grad,variable in grads_and_vars]
 gradient_placeholders=[]
 grads_and_vars_feed=[]
-for	grad,variable	in	grads_and_vars:
+for grad,variable in grads_and_vars:
 	gradient_placeholder=tf.placeholder(tf.float32,shape=grad.get_shape())
 	gradient_placeholders.append(gradient_placeholder)
 	grads_and_vars_feed.append((gradient_placeholder,variable))
-training_op	=	optimizer.apply_gradients(grads_and_vars_feed)
-init	=	tf.global_variables_initializer()
-saver	=	tf.train.Saver()
-#print discount_rewards([10,	0,	-50],discount_rate=0.8)
+training_op=optimizer.apply_gradients(grads_and_vars_feed)
+init=tf.global_variables_initializer()
+saver=tf.train.Saver()
+#print discount_rewards([10,0,-50],discount_rate=0.8)
 #print discount_and_normalize_rewards([[10,0,-50],[10,20]],discount_rate=0.8)
 
 
@@ -56,32 +56,32 @@ save_iterations=10
 discount_rate=0.95
 with tf.Session() as sess:
 	init.run()
-	for	iteration in range(n_iterations):
+	for iteration in range(n_iterations):
 		all_rewards=[]				
 		all_gradients=[]		
 		print "Epoch "+str(iteration+1)
 		env=gym.make('CartPole-v0')
-		for	game in	range(n_games_per_update):
+		for game in range(n_games_per_update):
 			current_rewards=[]			
 			current_gradients=[]	
 			obs=env.reset()
-			for	step in range(n_max_steps):
+			for step in range(n_max_steps):
 				env.render()
 				action_val,	gradients_val=sess.run([action,gradients],feed_dict={X:obs.reshape(1,n_inputs)})
 				obs,reward,done,info=env.step(action_val[0][0])
 				current_rewards.append(reward)
 				current_gradients.append(gradients_val)
-				if	done:
+				if done:
 					break
 			all_rewards.append(current_rewards)
 			all_gradients.append(current_gradients)
 			all_rewards=discount_and_normalize_rewards(all_rewards,discount_rate=0.8)
 			feed_dict={}
-			for	var_index,grad_placeholder in enumerate(gradient_placeholders):
-				mean_gradients=np.mean([reward*all_gradients[game_index][step][var_index] for game_index,rewards in	enumerate(all_rewards) for step,reward in enumerate(rewards)],axis=0)
+			for var_index,grad_placeholder in enumerate(gradient_placeholders):
+				mean_gradients=np.mean([reward*all_gradients[game_index][step][var_index] for game_index,rewards in enumerate(all_rewards) for step,reward in enumerate(rewards)],axis=0)
 				feed_dict[grad_placeholder]=mean_gradients
 			print "Episode "+str(game+1)
 			sess.run(training_op,feed_dict=feed_dict)
-			if	iteration%save_iterations==0:
+			if iteration%save_iterations==0:
 				saver.save(sess,"./my_policy_net_pg.ckpt")
 		env.close()
